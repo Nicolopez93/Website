bde0462e386b7b279d9dfffac8fa3228f597d07a
Nicolopez93 <nicolopezarq@gmail.com>
2024-04-10 18:17:24 -0300

    about ready

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import index from './index.css'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import About from './Components/About'
 function App() {
 
   return (
     <>
     <Navbar/>
     <Hero/>
+    <About/>
     </>
   )
 }

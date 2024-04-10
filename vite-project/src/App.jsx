8e48b8962757bff65d1b660a24c8efcc1337c961
Nicolopez93 <nicolopezarq@gmail.com>
2024-04-10 18:45:39 -0300

    support

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,7 @@ import index from './index.css'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import About from './Components/About'
+import Support from './Components/Support'
 function App() {
 
   return (
@@ -10,6 +11,7 @@ function App() {
     <Navbar/>
     <Hero/>
     <About/>
+    <Support/>
     </>
   )
 }

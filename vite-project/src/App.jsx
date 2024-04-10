7a2ecc6f3116a315a4f132a0c96259efe40608b4
Nicolopez93 <nicolopezarq@gmail.com>
2024-04-10 18:05:27 -0300

    navBar

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import index from './index.css'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    </>
+  )
+}
+
+export default App

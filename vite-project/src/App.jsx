2a9eb8a7cc4e972d3ca5e8c0421ae2ebd99d4b73
Nicolopez93 <nicolopezarq@gmail.com>
2024-04-12 18:06:52 -0300

    Pricing and All In One

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import About from './Components/About'
 import Support from './Components/Support'
+import AllInOne from './Components/AllInOne'
+import Pricing from './Components/Pricing'
 function App() {
 
   return (
@@ -12,6 +14,8 @@ function App() {
     <Hero/>
     <About/>
     <Support/>
+    <AllInOne/>
+    <Pricing/>
     </>
   )
 }
